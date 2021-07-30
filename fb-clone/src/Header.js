b7cff9273d7dfa227b6db0ef24de5d64f789c3ac
beingrishisharma <beingrishisharma@gmail.com>
2021-07-30 14:56:14 +0530

    init

---
+++ fb-clone/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '/Header.css'
+function Header() {
+    return (
+        <div className="header">
+            d
+            <div className="header__left"></div>
+            <div className="header__middle">
+
+            </div>
+            <div className="header__right"></div>
+            
+        </div>
+    )
+}
+
+export default Header
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCfOhFwmCtnx_3eBaRkqHRgar9bPmUl4p4",
+    authDomain: "facebook-clone-6293e.firebaseapp.com",
+    projectId: "facebook-clone-6293e",
+    storageBucket: "facebook-clone-6293e.appspot.com",
+    messagingSenderId: "229647422201",
+    appId: "1:229647422201:web:9b3dcc5c7c9212636e4828",
+    measurementId: "G-CYYQM2T1TK"
+  };
