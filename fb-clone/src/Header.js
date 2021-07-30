b43598f902c263fb525a01862bf082b73b285658
beingrishisharma <beingrishisharma@gmail.com>
2021-07-30 16:44:43 +0530

    init

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -21,12 +21,12 @@ function Header() {
         />
         <div className="header__input">
           <SearchIcon />
-          <input type="text"></input>
+          <input type="text" placeholder="Search Facebook"></input>
         </div>
       </div>
 
-      <div className="header__middle">
-        <div className="header__option">
+      <div className="header__center">
+        <div className="header__option header__option--active">
           <HomeIcon fontSize="large" />
         </div>
         <div className="header__option">
